87f3a0e53d715d395730793ec3748ed484dbfcc4
RatnikovMaxim <105432463+RatnikovMaxim@users.noreply.github.com>
2024-07-22 21:02:02 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,7 @@
+completed_tasks = 'всего задач:'
+a = 12
+spent_of_hours = 'затрачено часов:'
+b = 1.5
+course_name = 'Python'
+c = 'среднее время выполнения'
+print('Курс:', course_name, ',', completed_tasks, a,',',  spent_of_hours, b, ',', c, b/a)
