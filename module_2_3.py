712ec112d76a418551e5b694b4a8021e45daef21
RatnikovMaxim <105432463+RatnikovMaxim@users.noreply.github.com>
2024-08-04 15:57:19 +0300

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -1,9 +1,8 @@
 my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 initialValue = 0
 while initialValue < len(my_list):
-    if my_list[initialValue] < initialValue:
-        initialValue += 1
-        continue
-    else:
+    if my_list[initialValue] < 0:
+        break
+    if my_list[initialValue] > 0:
         print(my_list[initialValue])
-        initialValue += 1
+    initialValue += 1
